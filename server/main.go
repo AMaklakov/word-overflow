df8f347c2a39295d2ea84e8ea7a9eec7b608aab9
mav <ar.maklakov@gmail.com>
2023-08-20 12:30:34 +0300

    Rename frontend to client

--- server/main.go
+++ server/main.go
@@ -1,16 +1,22 @@
 package main
 
 import (
+	"AMaklakov/word-overflow/common"
 	"AMaklakov/word-overflow/word_overflow"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 )
 
-const PORT = ":5123"
+const PORT = "0.0.0.0:5000"
 
 func main() {
 	r := chi.NewRouter()
 	word_overflow.NewRouter(r, "words-overflow")
+	r.Get("/ping", func(w http.ResponseWriter, r *http.Request) {
+		common.WriteJSON(w, 200, &struct {
+			Message string `json:"message"`
+		}{"Pong"})
+	})
 	http.ListenAndServe(PORT, r)
 }
