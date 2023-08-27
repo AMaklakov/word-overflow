92ce083a010f335c560d07fad921358a26db9206
mav <ar.maklakov@gmail.com>
2023-08-27 18:56:26 +0300

    Update and refactor Game tests

--- server/word_overflow/game_test.go
+++ server/word_overflow/game_test.go
@@ -1,79 +1,88 @@
 package word_overflow
 
 import (
+	"AMaklakov/word-overflow/analyzers"
 	"strings"
+	"sync"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-// func TestGame(t *testing.T) {
-// 	game := NewGame("1", &Config{
-// 		Words:   20,
-// 		Players: 1,
-// 		Timeout: 0,
-// 	})
-// 	word := game.Words[len(game.Words)-1]
-// 	player := game.AddPlayer()
-// 	wg := sync.WaitGroup{}
+func TestGame(t *testing.T) {
+	game := NewGame("1", &Config{
+		Words:   20,
+		Players: 1,
+		Timeout: 0,
+	})
+	game.Status = statusRunning // Start the game
 
-// 	wg.Add(1)
-// 	go func() {
-// 		for i := 0; i < len(word.Text); i++ {
-// 			<-player.Ch
-// 		}
-// 		wg.Done()
-// 	}()
+	word := game.Words[len(game.Words)-1]
+	player := game.AddPlayer()
+	player.analyzers = make([]analyzers.Analyzer, 0) // reset analyzers so not influence test
 
-// 	for i := 0; i < len(word.Text); i++ {
-// 		game.EventsCh <- &GameMessage{
-// 			Player:  player.Color,
-// 			Type:    ClientTypeKey,
-// 			Payload: string(word.Text[i]),
-// 		}
-// 	}
+	wg := sync.WaitGroup{}
 
-// 	wg.Wait()
-// 	if !word.IsFinished() {
-// 		t.Error("Word is not written", word.Text, word.Written)
-// 	}
-// }
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for i := 0; i < len(word.Text); i++ {
+			<-player.Ch
+		}
+	}()
 
-// func TestGameMultiplePlayersSameWord(t *testing.T) {
-// 	N := 5
-// 	wg := sync.WaitGroup{}
+	for i := 0; i < len(word.Text); i++ {
+		game.EventsCh <- &GameMessage{
+			Player:  player.Color,
+			Type:    ClientTypeKey,
+			Payload: string(word.Text[i]),
+		}
+	}
 
-// 	game := NewGame("1", &Config{Words: 20, Players: 2, Timeout: 0})
-// 	word := game.Words[len(game.Words)-1]
+	wg.Wait()
+	assert.Equal(t, true, word.IsFinished(), "word is not written")
+}
 
-// 	for i := 0; i < N; i++ {
-// 		player := game.AddPlayer()
-// 		wg.Add(1)
-// 		go func() {
-// 			for i := 0; i < len(word.Text); i++ {
-// 				<-player.Ch
-// 			}
-// 			wg.Done()
-// 		}()
-// 	}
+func TestGameMultiplePlayersSameWord(t *testing.T) {
+	N := 5
+	wg := sync.WaitGroup{}
 
-// 	for i := 0; i < N; i++ {
-// 		wg.Add(1)
-// 		go func(i int) {
-// 			for j := 0; j < len(word.Text); j++ {
-// 				game.EventsCh <- &GameMessage{
-// 					Player:  game.Players[i].Color,
-// 					Type:    ClientTypeKey,
-// 					Payload: string(word.Text[j]),
-// 				}
-// 			}
-// 			wg.Done()
-// 		}(i)
-// 	}
+	game := NewGame("test game", &Config{Words: 20, Players: N, Timeout: 0})
+	game.Status = statusRunning // running the game
 
-// 	wg.Wait()
-// 	assert.Equal(t, true, word.IsFinished())
-// }
+	word := game.Words[len(game.Words)-1]
+
+	for i := 0; i < N; i++ {
+		wg.Add(1)
+
+		player := game.AddPlayer()
+		player.analyzers = make([]analyzers.Analyzer, 0) // reset analyzers
+
+		go func() {
+			defer wg.Done()
+			for i := 0; i < len(word.Text); i++ {
+				<-player.Ch
+			}
+		}()
+	}
+
+	for i := 0; i < N; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			for j := 0; j < len(word.Text); j++ {
+				game.EventsCh <- &GameMessage{
+					Player:  game.Players[i].Color,
+					Type:    ClientTypeKey,
+					Payload: string(word.Text[j]),
+				}
+			}
+		}(i)
+	}
+
+	wg.Wait()
+	assert.Equal(t, true, word.IsFinished())
+}
 
 func TestTypeKey(t *testing.T) {
 	game := NewGame("1", &Config{Words: 30, Players: 2, Timeout: 0})
@@ -85,12 +94,13 @@ func TestTypeKey(t *testing.T) {
 	}
 
 	for i, word := range game.Words {
-		color, finished := "", false
 		if i == 15 || i == 20 {
-			color, finished = "red", true
+			assert.Equal(t, true, word.IsFinished())
+			assert.Equal(t, "red", word.Color)
+		} else {
+			assert.Equal(t, false, word.IsFinished())
+			assert.Equal(t, "", word.Color)
 		}
-		assert.Equal(t, finished, word.IsFinished())
-		assert.Equal(t, color, word.Color)
 	}
 }
 
